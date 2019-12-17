4707518e66e90fe7bb8be86c3b1f6c25d84e107e
psujit10 <psujit10@gmail.com>
2019-12-17 07:33:54 +0530

    Initial Commit

---
+++ SujitIOSTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SujitIOSTest
+//
+//  Created by Admin on 12/17/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
